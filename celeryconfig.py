fc6f1726b6bbbcba19e7f9a9f608ea608ac2fbc9
Liza <poltavina.liza@yandex.ru>
2021-04-18 14:01:00 +0300

    f

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,5 +1,7 @@
-broker_url = 'redis://localhost:6379/0'
-result_backend = 'redis://localhost:6379/0'
+broker_url = 'redis://:redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com@' \
+             'gfwnMIT2nqMK5rqy9LgtCyPkx8RjevLx:15293/0'
+result_backend = 'redis://:redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com@' \
+             'gfwnMIT2nqMK5rqy9LgtCyPkx8RjevLx:15293/0'
 timezone = 'europe/moscow'
 track_started = True
 send_events = True
