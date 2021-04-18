de69affd1cc63ec3328cef8071959317e5cb9889
Hipoppoltark <48064845+Hipoppoltark@users.noreply.github.com>
2021-04-18 15:27:42 +0300

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,5 +1,5 @@
-broker_url = 'redis://redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com:15293/0'
-result_backend = 'redis://redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com:15293/0'
+broker_url = 'redis://:gfwnMIT2nqMK5rqy9LgtCyPkx8RjevLx@redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com:15293/0'
+result_backend = 'redis://:gfwnMIT2nqMK5rqy9LgtCyPkx8RjevLx@redis-15293.c256.us-east-1-2.ec2.cloud.redislabs.com:15293/0'
 timezone = 'europe/moscow'
 track_started = True
 send_events = True
